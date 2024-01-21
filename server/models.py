2e3dc1ddf29666b4173ee64555f9a3554564ecd1
Matthew <+>
2024-01-21 08:51:09 +0300

    validations lab

--- server/models.py
+++ server/models.py
@@ -15,6 +15,21 @@ class Author(db.Model):
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+    
+    @validates('name')
+    def validate_name(self,key,name):
+        if not name:
+            raise ValueError("Name field is required.")
+        author = db.session.query(Author.id).filter_by(name=name).first()
+        if author is not None:
+            raise ValueError("Name must be unique")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self,key,phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError("Phone number must be 10 digits.")
+        return phone_number 
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -32,3 +47,27 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+
+    @validates('title')
+    def validate_title(sefl,key, title):
+        if not title:
+            raise ValueError("Title field is required")
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError("No clickbait found")
+        return title
+    
+    @validates('content', 'summary')
+    def validate_length(self, key, string):
+        if key == 'content' and len(string) < 250:
+            raise ValueError("Post content must be greater than or equal to 250 characters")
+        elif key == 'summary' and len(string) > 250:
+            raise ValueError("Post summary must be less than or equal to 250 characters long")
+            
+        return string
+    @validates('category')
+    def validate_category(self, key, category):
+        valid_categories = ['Fiction', 'Non-fiction']
+        if category not in valid_categories:
+            raise ValueError("Category must be Fiction or Non-fiction.")
+        return category
